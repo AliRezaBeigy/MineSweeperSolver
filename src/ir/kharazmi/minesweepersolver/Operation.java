4afabfa8d3d973bd07784a2e01dc72250ac22888
mmahdisonic <m.mahdi_sonic@yahoo.com>
2019-04-02 11:52:39 +0430

    flag added

--- src/ir/kharazmi/minesweepersolver/Operation.java
+++ src/ir/kharazmi/minesweepersolver/Operation.java
@@ -4,12 +4,14 @@ public class Operation {
     int width, height;
     int[][] table;
     boolean[][] click;
+    boolean[][] flag;
 
     public Operation(int width, int height, int[][] table) {
         this.width = width;
         this.height = height;
         this.table = new int[width][height];
         this.click = new boolean[width][height];
+        this.flag = new boolean[width][height];
         for (int i = 0; i < width; i++)
             System.arraycopy(table[i], 0, this.table[i], 0, height);
     }
@@ -91,14 +93,35 @@ public class Operation {
         if (x < width - 1 && table[x + 1][y] == -1)
             click[x + 1][y] = true;
         if (x < width - 1 && y < height - 1 && table[x + 1][y + 1] == -1)
-            click[x + 1][y + 1] = true;
+            clicflagk[x + 1][y + 1] = true;
+    }
+    
+    void flagAdj(int x, int y) {
+        if (x > 0 && y > 0 && table[x - 1][y - 1] == -1)
+            flag[x - 1][y - 1] = true;
+        if (y > 0 && table[x][y - 1] == -1)
+            flag[x][y - 1] = true;
+        if (x > 0 && table[x - 1][y] == -1)
+            flag[x - 1][y] = true;
+        if (x < width - 1 && y > 0 && table[x + 1][y - 1] == -1)
+            flag[x + 1][y - 1] = true;
+        if (x > 0 && y < height - 1 && table[x - 1][y + 1] == -1)
+            flag[x - 1][y + 1] = true;
+        if (y < height - 1 && table[x][y + 1] == -1)
+            flag[x][y + 1] = true;
+        if (x < width - 1 && table[x + 1][y] == -1)
+            flag[x + 1][y] = true;
+        if (x < width - 1 && y < height - 1 && table[x + 1][y + 1] == -1)
+            flag[x + 1][y + 1] = true;
     }
 
     void mineFinder() {
         for (int i = 0; i < width; i++)
             for (int j = 0; j < height; j++)
-                if (table[i][j] > 0 && getAdj(i, j, -1) > 0 && getAdj(i, j, -1) + getAdj(i, j, -2) == table[i][j])
+                if (table[i][j] > 0 && getAdj(i, j, -1) > 0 && getAdj(i, j, -1) + getAdj(i, j, -2) == table[i][j]) {
                     setAdj(i, j, -1, -2);
+                    flagAdj(i, j);
+                }
     }
 
     void clickFinder() {
